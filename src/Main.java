bd8e1812b047365ef0c2852467b1b6854d145c9f
Skop Pavel <skop@gymvod.local>
2023-10-18 15:26:11 +0200

    prvni outline projektu - todo komentare

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//predpokladejme, ze robot zacina pred modrou krychli
+		
+		
+		
+		//TODO
+		/*
+		 * trackovani casu a vzdalenosti
+		 * promenne pro dva plany a pak pred startem prepinat podle planu
+		 * 	(protoze plan nemusi byt na hristi nalepen spravne)
+		 * 
+		 * */
+		
+		
 	}
-
+	public static void go(int range)
+	{
+		
+	}
+	
+	public static void turnLeft(int backupLenght)
+	{
+		
+	}
 }
