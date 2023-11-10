855b9cf0de76c918c881e1ec3feba0dcd77d3857
Pavel <pskop.k10@gmail.com>
2023-11-10 12:30:09 +0100

    funkce update a resetDist

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
 import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.Button;
+import lejos.hardware.motor.Motor;
+import lejos.utility.Delay;
 
 
 public class Main {
 	//nastavime si globalni promenne
 	public static double time = 0;
 	public static double dist = 0;
-
+	public static long startTime = System.currentTimeMillis();
+	public static int initialDistLeft = Motor.A.getTachoCount();
+	public static int initialDistRight = Motor.D.getTachoCount();
+	public static double r = 0.056;
+	public static double wallDist;
+	public static int cubeNum = 0;
+	public static int wallNum = 0;
 	public static void main(String[] args) {
 		//predpokladejme, ze robot zacina pred modrou krychli
 
@@ -26,15 +39,29 @@ public class Main {
 
 	}
 
-	public static void update(int velocity, double dt)
+	public static void update()
 	{
-		//update hodnot time a dist
-		// dt je delta time
-		time += dt;
-		dist += velocity/dt;
-		//motor tacho by mel fungovat lepe
+		//update hodnot time, dist a wallDist
 
+        long currentTime = System.currentTimeMillis();
+        time = currentTime - startTime;
 
+        // vyýpoèet ujeté vzdálenosti
+        int currentTachoCountLeft = Motor.A.getTachoCount();
+        int currentTachoCountRight = Motor.D.getTachoCount();
+        int distanceDgLeft = currentTachoCountLeft - initialDistLeft;
+        int distanceDgRight = currentTachoCountRight - initialDistRight;
+        double distDg = (distanceDgLeft + distanceDgRight) / 2.0; // prùmìrná ujetá vzdálenost ve stupních
+        dist = Math.PI/180 * r * distDg; // prumnerna ujeta vzdalenost v metrech
+
+        wallDist = getDistanceValue();
+	}
+
+	public static void resetDist()
+	{
+		//nastaví prvotní ujetou vzdálenost na aktulání - reset ujeté vzdálenosti
+		initialDistLeft = Motor.A.getTachoCount();
+		initialDistRight = Motor.D.getTachoCount();
 	}
 
 	public static void go()
